44f5829b8cf2182f2a8ce30e6c7ed03c5f7f274e
bobziemerman <bobzim17@gmail.com>
2018-04-29 13:39:43 -0500

    did most of the landing page

--- js/index.js
+++ js/index.js
@@ -2,5 +2,8 @@ var app = angular.module('harmonicIntervals', ['isteven-multi-select']);
 app.controller('main', function($scope) {
     console.log('sanity');
 
+    $scope.winWidth = $(window).width();
+    $scope.docWidth = $(document).width();
+
 });
 
