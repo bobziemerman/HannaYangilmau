e883173d94c8d08a689324157c2eb07b47949c46
Bob <bobziemerman@gmail.com>
2018-08-07 19:06:19 -0500

    added about page

---
+++ db/about.php
@@ -0,0 +1,35 @@
+<?php
+$servername = "localhost";
+$username = "hannay_admin";
+$password = "Hanna1234";
+$dbname = "hannay_data";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT * FROM about";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    echo "[";
+    $first = true;
+    while($row = $result->fetch_assoc()) {
+        if($first){
+            $first = false;
+        } else {
+            echo ", ";
+        }
+        echo json_encode($row);
+    }
+    echo "]";
+} else {
+    echo "[]";
+}
+$conn->close();
+?>
---
+++ js/about.js
@@ -0,0 +1,22 @@
+var app = angular.module('hanna', ['isteven-multi-select']);
+app.controller('main', function($scope, $http, $sce) {
+    console.log('sanity');
+$scope.width = $(window).width();
+
+    //Get about data
+    setData('about', 'about.php', function(){console.log($scope.about);});
+
+    /************** Helper Functions ***************/
+
+    function setData(field, phpPage, thenFunction){
+        $http.get('db/'+phpPage)
+        .then(function (response) {
+            $scope[field] = response.data;
+            if(thenFunction){
+                thenFunction();
+            }
+        });
+    }
+
+});
+
